3617edfe4a5301e533029ce4a24d57d67cd663f5
AkkU438 <109703098+AkkU438@users.noreply.github.com>
2026-05-11 20:29:54 -0400

    1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,17 +4,28 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [allClicks, setAll] = useState(0);
+  const [value, setValue] = useState(0);
 
   const handleGoodClick = () => {
-    setGood(good + 1)
+    setGood(good + 1);
+    setAll(allClicks + 1);
+    setValue(value + 1);
   }
   const handleNeutralClick = () => {
-    setNeutral(neutral + 1)
+    setNeutral(neutral + 1);
+    setAll(allClicks + 1);
+    setValue(value + 0);
   }
   const handleBadClick = () => {
     setBad(bad + 1);
+    setAll(allClicks + 1);
+    setValue(value - 1);
   }
 
+  let average = value / allClicks;
+  let positive = good/allClicks;
+  
   return(
     <div>
       <h1>Give Feedback</h1>
@@ -25,6 +36,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {allClicks}</p>
+      <p>average {average}</p>
+      <p>positive {positive}</p>
     </div>
   )
 }
